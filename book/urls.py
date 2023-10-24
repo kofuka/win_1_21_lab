36c69f0dcb34f1b19224c794ebdbd747aa8ee611
kofuka <bermetkartan@gmail.com>
2023-10-24 21:21:32 +0600

    create and delete

--- book/urls.py
+++ book/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('book/', views.book_view),
     path('book_detail/<int:id>/', views.book_detail_view),
+    path('add-cooment/', views.createBookView),
+    path('create_post_book/', views.createBookPostView),
+    path('book_list/', views.book_delete_view),
+    path('book_list/<int:id>/delete/', views.book_drop_view),
 ]
--- main/settings.py
+++ main/settings.py
@@ -105,9 +105,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
